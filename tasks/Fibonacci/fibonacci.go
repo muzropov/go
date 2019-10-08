2ac1c7d823a23577a3398f80cc56a53048455af0
muzropov <muzropovoybek@gmail.com>
2019-10-08 11:30:12 +0500

    fibonacci

---
+++ tasks/Fibonacci/fibonacci.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//simple fibonacci
+func fibonacci1(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	} else {
+		return fibonacci1(n - 1) + fibonacci1(n - 2)
+	}
+}
+
+//fibonacci which starting x, y
+func fibonacci2(x, y, n int) int {
+	if n == 1 {
+		return x
+	} else if n == 2 {
+		return y
+	} else {
+		return fibonacci2(y, x + y, n - 1)
+	}
+}
+
+func main() {
+	fmt.Println(fibonacci1(10))
+	fmt.Println(fibonacci2(1,1,10))
+}
